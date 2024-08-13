0336d25a382dbdfe9fe3c6c9219a57d3dcd9cbfc
Mubtasim <mubtasimf443@gmail.com>
2024-08-13 17:05:58 +0600

    usetoggle create

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-<h1>Here is the react custom hooks</h2>
+<h2>Here is the react custom hooks</h2>
+
+<h3>It contains a lot of react custom hook that you can you in in your project</h3>
+ 
+
+ <h2 style="color:red">Hooks </h2>
+<style>
+    li {
+        color :rgb(15, 80, 201);
+        font-weight: 600;
+        font-size: 1em;
+    }
+</style>
+<ul>
+    <li >useLocalStorage</li>
+    <li>UseToggle</li>
+</ul>
---
+++ useToggle.jsx
@@ -0,0 +1,25 @@
+/* بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ  ﷺ  InshaAllah */
+
+
+
+import React, { useState } from 'react'
+
+const useToggle = (defaultValue) => {
+    let [state , setState] = useState(defaultValue !== undefined ? defaultValue : false)
+
+    function toggle(params) {
+        setState(prev => 
+            typeof  params === 'boolean' ? params : !prev 
+        )
+    }
+
+    return [state, toggle]
+}
+
+export default useToggle
+
+
+
+
+
+
